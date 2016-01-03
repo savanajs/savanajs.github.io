43a37c2ad1fdb272afc0f1f9c8ffc28430798a13
savanajs <projetosavanajs@gmail.com>
2016-01-03 17:06:17 -0200

    body fade

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,12 +2,11 @@ var Savana = {
 	selectorBtnMenuMobile: "#menu-mobile",
 	selectorMenuMobile: "#bx-menu",
 	init: function() {
-
 		var _self = this;
 		_self.initLoaderPader();
 		_self.showLoaderPage();
 		_self.controlMenuMobile(_self);
-
+		_self.controlBtnBodyFadeOut(_self);
 	},
 	initLoaderPader: function() {
 		savana.loadPreloader();
@@ -19,43 +18,41 @@ var Savana = {
 		savana.hidePreloader();
 	},
 	controlMenuMobile: function(_self){
-
         $savana(_self.selectorBtnMenuMobile).on("click", function(){
         	var $this = $savana(_this);
         	$this.toggleClass("active");
-
         	if($this.hasClass("active")){
                _self.openMenuMobile(_self);
         	}else{
                _self.closeMenuMobile(_self);
         	}
-
         });
-
 	},
 	openMenuMobile: function(_self){
- 
         $savana(_self.selectorMenuMobile).show();
         $savana(_self.selectorMenuMobile).removeClass("bounceOutLeft");
         $savana(_self.selectorMenuMobile).addClass("bounceInLeft");
-
 	},
 	closeMenuMobile: function(_self){
-
         $savana(_self.selectorMenuMobile).removeClass("bounceInLeft");
 		$savana(_self.selectorMenuMobile).addClass("bounceOutLeft");
-
+	},
+	bodyFadeOut: function(){
+		$savana("body").removeClass("fadeIn");
+		$savana("body").addClass("fadeOut");
+	},
+	controlBtnBodyFadeOut: function(_self){
+		$savana("a.btn-body-fade").on("click", function(e){
+           _self.bodyFadeOut();
+           e.preventDefault();
+		});
 	}
 }
 
 $savana(document).done(function(e) {
-
     Savana.init();
-
 });
 
 $savana(window).onload(function(e) {
-
 	Savana.hideLoaderPage();
-
 });
